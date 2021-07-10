c8e26d1b3381f5bfc008d9908e90bf0b8bdf6b20
ydk1204 <ejrbdi@naver.com>
2021-07-10 18:44:35 +0900

    Input-Add

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,32 +1,42 @@
-import React, { Component } from 'react';
-import Habit from './habit';
+import React, { Component } from "react";
+import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
+  };
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
+  };
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  };
+  handleAdd = (name) => {
+    this.props.onAdd(name);
+  };
 
-    handleIncrement = (habit) => {
-        this.props.onIncrement(habit);
-    };
-    handleDecrement = (habit) => {
-        this.props.onDecrement(habit);
-    };
-    handleDelete = (habit) => {
-        this.props.onDelete(habit);
-    };
-    render() {
-        return (
-            <ul>
-                {
-                    this.props.habits.map(habit => (
-                        <Habit key={habit.id} habit={habit} 
-                        onIncrement={this.handleIncrement}
-                        onDecrement={this.handleDecrement}
-                        onDelete={this.handleDelete}
-                        />
-                    ))
-                }
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <div className="habits">
+        <HabitAddForm onAdd={this.handleAdd} />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Habits;
