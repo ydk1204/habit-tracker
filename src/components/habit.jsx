4a5a5315b451aa2c21c57d0ce2f29866cf7ea1b9
ydk1204 <ejrbdi@naver.com>
2021-07-12 19:33:17 +0900

    PureComponent

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from "react";
 
-class Habit extends Component {
-    handleIncrement = () => {
-        this.props.onIncrement(this.props.habit);
-    };
-    handleDecrement = () => {
-        this.props.onDecrement(this.props.habit);
-    };
-    handleDelete = () => {
-        this.props.onDelete(this.props.habit);
-    };
-    render() {
-        console.log(this.props);
-        const {name, count} = this.props.habit;
+class Habit extends PureComponent {
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+  render() {
+    console.log(this.props);
+    const { name, count } = this.props.habit;
 
-        return (<li className="habit"> 
+    return (
+      <li className="habit">
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
-            <i className="fas fa-plus"></i>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus"></i>
         </button>
-        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
-            <i className="fas fa-minus"></i>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus"></i>
         </button>
-        <button className="habit-button habit-delete" onClick={this.handleDelete}>
-            <i className="fas fa-trash"></i>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
         </button>
-        </li>
-        );
-    }
+      </li>
+    );
+  }
 }
 
 export default Habit;
--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,27 +1,27 @@
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 
-class HabitAddForm extends PureComponent {
-  inputRef = React.createRef();
-  onSubmit = (event) => {
+const HabitAddForm = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+
+  const onSubmit = (event) => {
     event.preventDefault();
-    const name = this.inputRef.current.value;
-    name && this.props.onAdd(name);
-    this.inputRef.current.value = "";
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    inputRef.current.value = "";
   };
 
-  render() {
-    return (
-      <form className="add-form" onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Habit"
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
 
 export default HabitAddForm;
